6ec5c45a341a877a4af879b766ad435bb29f6575
juliannavenczel063-code <julianna.venczel063@students.cmcss.net>
2025-12-18 12:31:35 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,19 @@
+total_candy = bagA + bagB + bagC
+print(f"Total candy collected: {total_candy}")
 
+
+# Part 2: Fair sharing (include yourself)
+people += 1
+share = total_candy//people
+leftover = total_candy % people
+print(f"Each person gets: {share}")
+print(f"leftover candy: {leftover}")
+
+
+# Part 3: Include the sick friend
+# Variable reassignment is fine - previous values were already printed
+people += 1
+share = total_candy//people
+leftover = total_candy % people
+print(f"Each person gets: {share}")
+print(f"Leftover candy: {leftover}")
